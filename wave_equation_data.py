0ec07685fa7e02ef54deee2a1c630314c7228800
EC <ecalzolaio@gmail.com>
2019-01-21 09:19:37 -0500

    Updated comments, removed xticklabels

--- wave_equation_data.py
+++ wave_equation_data.py
@@ -18,7 +18,7 @@ df = pd.read_csv('Data.csv')
 xyz = np.array(df[['x','t','z']])
 afont = {'fontname':'Arial'}
 
-# reshaping arrays to function with matplotlib
+# reshaping into 2D arrays to function with matplotlib
 x = xyz[:,0]
 X = np.reshape(x, (33, 11))
 y = xyz[:,1]
@@ -33,10 +33,8 @@ ax = fig.add_subplot(111, projection='3d')
 ax.set_xlabel('Distance', size = 15, labelpad=10, **afont)
 ax.set_ylabel('Time', size = 15, labelpad=10, **afont)
 ax.set_zlabel('Displacement', size=15, labelpad=5, **afont)
-#ax.set_xticks([-4.00,-3.75,-3.50,-3.25,-3.00,-2.75,-2.50,-2.25,-2.00,-1.75,-1.75,-1.50,-1.25,-1.00,-0.75,-0.50,-0.25,
-            #  0.00,0.25,0.50,0.75,1.00,1.25,1.50,1.75,2.00,2.25,2.50,2.75,3.00,3.25,3.50,3.75,4.00])
 
-# set the tick size - set y and z first, then rotate x
+# set the tick size - set y and z first, then rotate x if needed
 ax.tick_params(labelsize=10)
 #ax.tick_params(axis='x', labelrotation=70)
 
@@ -51,6 +49,6 @@ for tick in ax.get_zticklabels():
     tick.set_fontname("Arial")
 
 ax.plot_surface(X,Y,Z, rstride = 1, cstride = 1, cmap='hot', antialiased=True, shade=False)
-plt.savefig('heatMapGraph.png', dpi=600, bbox_inches='tight')
+#plt.savefig('heatMapGraph.png', dpi=600, bbox_inches='tight')
 plt.show()
 
